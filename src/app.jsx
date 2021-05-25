461dc47ed1c17122b96d11515bd46e7bb04fdb3b
Seongjin Yoo <yoosngj@gmail.com>
2021-05-25 23:58:10 +0900

    Make search header functions

--- src/app.jsx
+++ src/app.jsx
@@ -5,7 +5,23 @@ import VideoList from "./components/video_list/video_list";
 
 function App() {
   const [videos, setVideos] = useState([]);
-  const [name, setName] = useState("oliver");
+  const search = query => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch(
+      `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyCUvB8Sz1eHI64qbp_-tgDsZWXFfXgZ6d0`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId }))
+      )
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
+  }
 
   useEffect(() => {
     console.log("useEffect");
@@ -25,7 +41,7 @@ function App() {
   }, []);
   return (
     <div className={styles.app}>
-      <SearchHeader />
+      <SearchHeader onSearch={search} />
       <VideoList videos={videos} />
     </div>
   );
