f59aa1ed2473ce859ec2f6e0abc73b5f97600c89
Seongjin Yoo <yoosngj@gmail.com>
2021-06-03 00:33:02 +0900

    Add responsive mode and clean up

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,8 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState } from 'react';
 import styles from './app.module.css';
+import VideoList from './components/video_list/video_list';
 import SearchHeader from './components/search_header/search_header';
-import VideoList from "./components/video_list/video_list";
-import VideoDetail from "./components/video_detail/video_detail";
+import VideoDetail from './components/video_detail/video_detail';
 
 function App({ youtube }) {
   const [videos, setVideos] = useState([]);
@@ -11,23 +11,18 @@ function App({ youtube }) {
   const selectVideo = (video) => {
     setSelectedVideo(video);
   };
-  
-  // useCallback은 한번 만들면 메모리 상에 계속 보관하기 때문에 많은 영항을 끼칠 수 있음.
-  // 자식 컴포넌트가 다시 리랜더링 안되게 하기 위해서만 사용할 때 좋음.
+
   const search = useCallback((query) => {
     setSelectedVideo(null);
     youtube
       .search(query) //
       .then((videos) => {
         setVideos(videos);
-        
       });
   }, [youtube]);
 
   useEffect(() => {
-    youtube
-    .mostPopular() // 
-    .then(videos => setVideos(videos));
+    youtube.mostPopular().then(videos => setVideos(videos));
   }, [youtube]);
   return (
     <div className={styles.app}>
@@ -42,7 +37,7 @@ function App({ youtube }) {
           <VideoList
             videos={videos}
             onVideoClick={selectVideo}
-            display={selectedVideo ? "list" : "grid"}
+            display={selectedVideo ? 'list' : 'grid'}
           />
         </div>
       </section>
